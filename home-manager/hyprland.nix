034edd5d41a5a43f37e47fbd7be78dd2d2bbb91d
Richard Yu <richard.yu@classpass.com>
2026-06-22 18:17:28 -0400

    add some useful hyprland config binds

--- home-manager/hyprland.nix
+++ home-manager/hyprland.nix
@@ -92,6 +92,16 @@ in
         "$mainMod SHIFT,k,movewindow,u"
         "$mainMod SHIFT,l,movewindow,r"
 
+        "$mainMod,F,fullscreen"
+        "$mainMod,SPACE,togglefloating"
+        "$mainMod,P,pin"
+        "$mainMod,G,togglesplit"
+
+        "$mainMod,comma,movewindow,mon:-1"
+        "$mainMod,period,movewindow,mon:+1"
+        "$mainMod SHIFT,comma,movecurrentworkspacetomonitor,mon:-1"
+        "$mainMod SHIFT,period,movecurrentworkspacetomonitor,mon:+1"
+
         "$mainMod,TAB,workspace,previous"
 
         "$mainMod,RETURN,exec,kitty"
@@ -99,7 +109,7 @@ in
         "$mainMod,D,exec,rofi -show drun -show-icons"
         "$mainMod SHIFT,S,exec,grim -g \"$(slurp)\" - | wl-copy"
 
-        "$mainMod,Q,killactive"
+        "$mainMod SHIFT,Q,killactive"
 
         "$mainMod,1,workspace,1"
         "$mainMod,2,workspace,2"
@@ -120,6 +130,18 @@ in
         "$mainMod SHIFT,7,movetoworkspace,7"
         "$mainMod SHIFT,8,movetoworkspace,8"
         "$mainMod SHIFT,9,movetoworkspace,9"
+      ];
+
+      bindm = [
+        "$mainMod,mouse:272,movewindow"
+        "$mainMod,mouse:273,resizewindow"
+      ];
+
+      binde = [
+        "$mainMod CTRL,h,resizeactive,-20 0"
+        "$mainMod CTRL,j,resizeactive,0 20"
+        "$mainMod CTRL,k,resizeactive,0 -20"
+        "$mainMod CTRL,l,resizeactive,20 0"
       ];
     };
 
